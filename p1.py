1a6e652cd6934e26ca09a45605a050921bfea2ac
Muhammad Taimoor Ajmal <taimoorajmal00@gmail.com>
2025-07-01 16:39:20 +0500

    myFirstCommit

---
+++ p1.py
@@ -0,0 +1,17 @@
+print("Hello World")
+print(5)
+
+print("This is my first Program\
+      yes this is my first Program") 
+
+print(12*44)
+
+# This is my first Program 
+# '''
+'''this is my first program 
+
+this is my first program 
+this is my first program 
+this is my first program 
+this is my first program '''
+ 
